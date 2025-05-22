1450175b299f689edd6c8c2f9f5d8f8d4dd58b38
Ubuntu <ubuntu@ip-172-31-38-189.ap-northeast-2.compute.internal>
2025-05-22 18:00:01 +0900

    append line 4

--- sample.py
+++ sample.py
@@ -2,3 +2,4 @@ print('Hello World!')
 """HTTP server classes.
 
 Note: BaseHTTPRequestHandler doesn't implement any HTTP request; see
+SimpleHTTPRequestHandler for simple implementations of GET, HEAD and POST.
